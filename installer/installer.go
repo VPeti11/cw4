ba3b9caf649b9b4833344d3b4c49d0a8e0a77252
VPeti11 <vpeti011@protonmail.com>
2025-07-20 10:51:35 +0200

    Added installer

---
+++ installer/installer.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"time"
+)
+
+// --- Constants and Config ---
+
+const (
+	ProgramName  = "cw4"
+	AuthorName   = "VPeti11"
+	GitRepoURL   = "https://gitlab.com/VPeti11/cw4.git"
+	LocalRepoDir = "cw4"
+)
+
+var Dependencies = map[string][]string{
+	"apt":    {"go", "git"},
+	"dnf":    {"go", "git"},
+	"pacman": {"go", "git"},
+}
+
+// --- Core Installer Functions ---
+
+func ClearScreen() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	_ = cmd.Run()
+}
+
+func CommandExists(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+	return err == nil
+}
+
+func DetectPackageManager() string {
+	switch {
+	case CommandExists("apt"):
+		return "apt"
+	case CommandExists("dnf"):
+		return "dnf"
+	case CommandExists("pacman"):
+		return "pacman"
+	default:
+		return ""
+	}
+}
+
+func InstallDependencies(manager string) error {
+	pkgs, ok := Dependencies[manager]
+	if !ok {
+		return fmt.Errorf("unsupported package manager: %s", manager)
+	}
+
+	switch manager {
+	case "apt":
+		_ = exec.Command("sudo", "apt", "update").Run()
+		args := append([]string{"apt", "install", "-y"}, pkgs...)
+		return exec.Command("sudo", args...).Run()
+	case "dnf":
+		args := append([]string{"dnf", "install", "-y"}, pkgs...)
+		return exec.Command("sudo", args...).Run()
+	case "pacman":
+		args := append([]string{"pacman", "-Syu", "--noconfirm"}, pkgs...)
+		return exec.Command("sudo", args...).Run()
+	}
+	return nil
+}
+
+func InstallPythonPackage(pkgName string, allowSystemBreak bool) error {
+	_ = exec.Command("python3", "-m", "pip", "install", "--upgrade", "pip").Run()
+
+	args := []string{"-m", "pip", "install", pkgName}
+	if allowSystemBreak {
+		args = append(args, "--break-system-packages")
+	}
+	return exec.Command("python3", args...).Run()
+}
+
+func InstallPythonScript(inputPath, outputName string) error {
+	outputPath := filepath.Join("/usr/bin", outputName)
+	header := []byte("#!/usr/bin/env python3\n")
+
+	if err := os.WriteFile(outputPath, header, 0755); err != nil {
+		return err
+	}
+
+	content, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err = f.Write(content); err != nil {
+		return err
+	}
+
+	return exec.Command("chmod", "+x", outputPath).Run()
+}
+
+func InstallGoBinary(sourceFile string, outName string) error {
+	outPath := filepath.Join("/usr/bin", outName)
+	cmd := exec.Command("go", "build", "-o", outPath, sourceFile)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return exec.Command("chmod", "+x", outPath).Run()
+}
+
+func CloneGitRepo(url string) error {
+	cmd := exec.Command("git", "clone", url)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func ChangeDirectory(dir string) error {
+	return os.Chdir(dir)
+}
+
+func PromptContinue(message string) {
+	ClearScreen()
+	fmt.Println(message)
+	fmt.Println("Press Enter to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func CheckLinuxPlatform() error {
+	if runtime.GOOS != "linux" {
+		return fmt.Errorf("this installer only supports Linux")
+	}
+	return nil
+}
+
+func SleepMessage(message string, duration time.Duration) {
+	fmt.Println(message)
+	time.Sleep(duration)
+}
+
+func ShowWelcomeMessage() {
+	ClearScreen()
+	fmt.Printf("Welcome to the %s installer\n", ProgramName)
+	fmt.Printf("Made by %s\n\n", AuthorName)
+	fmt.Println("Press Enter to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func main() {
+	// Clone repo
+	if err := CloneGitRepo(GitRepoURL); err != nil {
+		log.Fatalf("Failed to clone repo: %v", err)
+	}
+
+	// Change into cloned directory
+	if err := ChangeDirectory(LocalRepoDir); err != nil {
+		log.Fatalf("Failed to change directory: %v", err)
+	}
+
+	// Build main.go into /usr/bin/ProgramName
+	outPath := filepath.Join("/usr/bin", ProgramName)
+	cmd := exec.Command("go", "build", "-o", outPath, "main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to build binary: %v", err)
+	}
+}
